9b128a491091eb75474de80218186602d3407492
kickbuttowski80 <pazpaz25@gmail.com>
2020-01-27 16:31:09 -0800

    first-deploy

---
+++ app/javascript/app.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <p> {{message}} </p>
+    <v-btn class="primary" v-on:click="messageIs()"> what is message? </v-btn>
+    <v-alert
+      :value = "messageValue"
+      type = "success"
+    >
+      This is a success alert.
+    </v-alert>
+    <google-contacts></google-contacts>
+  </v-app>
+</template>
+
+
+<script>
+import GoogleContacts from './components/GoogleContacts'
+export default {
+  components:{
+    'google-contacts': GoogleContacts
+  },
+  data(){
+    return {
+        messageValue: false,
+        message: "Vuetify is here"
+    }  
+  },
+  methods:{
+    messageIs(){
+       this.messageValue == true ? this.messageValue =false : this.messageValue =true
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
